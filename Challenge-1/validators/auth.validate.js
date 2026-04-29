cdbd9f39ce7b99609629b08ef75a0872811c3936
FerventFrost <ferventfrost5@gmail.com>
2026-04-29 19:29:23 +0300

    feat(validators): Add login validation using express-validator for email and password

---
+++ Challenge-1/validators/auth.validate.js
@@ -0,0 +1,8 @@
+const { body } = require('express-validator');
+
+const loginValidator = [
+  body('email').isEmail().withMessage('Invalid email'),
+  body('password').notEmpty().withMessage('Password is required'),
+];
+
+module.exports = { loginValidator };
