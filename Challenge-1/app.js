7f8aeec3716d4d05cedda43e79185091b5d1a573
FerventFrost <ferventfrost5@gmail.com>
2026-04-29 17:37:24 +0300

    fix(app): Add eslint directive to suppress console log warning on server startup

--- Challenge-1/app.js
+++ Challenge-1/app.js
@@ -7,8 +7,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console -- startup log
   console.log(`Server listening on port ${PORT}`);
 });
 
