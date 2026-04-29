e47149ce52fb03e9ffb51fb454f50962c5d8e1e2
FerventFrost <ferventfrost5@gmail.com>
2026-04-29 20:06:15 +0300

    feat(middlewares): Implement authentication and authorization middleware for user access control

--- Challenge-1/middlewares/index.js
+++ Challenge-1/middlewares/index.js
@@ -1,10 +1,29 @@
 const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = process.env.JWT_SECRET;
+const { unauthorizedError, forbiddenError } = require('../utils/error.util');
 
 const validateRequest = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
+  next();
+};
+
+const authenticate = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token || !token.startsWith('Bearer ')) unauthorizedError('Unauthorized');
+
+  const decoded = jwt.verify(token.split(' ')[1], JWT_SECRET);
+  if (!decoded) unauthorizedError('Unauthorized');
+
+  req.user = decoded;
+  next();
+};
+
+const isAdmin = (req, res, next) => {
+  if (req.user.role !== 'admin') forbiddenError('Forbidden');
   next();
 };
 
@@ -22,10 +41,22 @@ const errorHandler = (err, req, res, next) => {
   if (err.name === 'UnauthorizedError') {
     return res.status(401).json({ errors: 'Unauthorized' });
   }
-  if (res.name === 'NotFoundError') {
-    return res.status(404).json({ message: 'Not Found' });
+  if (err.name === 'NotFoundError') {
+    return res.status(404).json({ errors: 'Not Found' });
+  }
+  if (err.name === 'ForbiddenError') {
+    return res.status(403).json({ errors: 'Forbidden' });
+  }
+  if (err.name === 'JsonWebTokenError' || err.name === 'TokenExpiredError') {
+    return res.status(401).json({ errors: 'Unauthorized' });
   }
   next(err);
 };
 
-module.exports = { validateRequest, errorHandler };
+module.exports = {
+  validateRequest,
+  authenticate,
+  isAdmin,
+  notFound,
+  errorHandler,
+};
