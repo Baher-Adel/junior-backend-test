8acbd37ada56c8ecb90e014139c2060bb9d54a66
FerventFrost <ferventfrost5@gmail.com>
2026-04-29 17:14:58 +0300

    feat(utils): Implement password hashing and comparison utilities using bcrypt

---
+++ Challenge-1/utils/hash.util.js
@@ -0,0 +1,17 @@
+const bcrypt = require('bcrypt');
+
+const SALT_ROUNDS = process.env.SALT_ROUNDS || 10;
+
+const hashPassword = async (password) => {
+  const salt = await bcrypt.genSalt(SALT_ROUNDS);
+  return await bcrypt.hash(password, salt);
+};
+
+const comparePassword = async (password, hash) => {
+  return await bcrypt.compare(password, hash);
+};
+
+module.exports = {
+  hashPassword,
+  comparePassword,
+};
