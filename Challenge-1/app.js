7a9cb433eb66e75f6d2b183382299e4d6fed9d45
FerventFrost <ferventfrost5@gmail.com>
2026-04-29 20:23:52 +0300

    feat(database): Integrate MongoDB connection and enable CORS support in the application

--- Challenge-1/app.js
+++ Challenge-1/app.js
@@ -4,10 +4,23 @@ const express = require('express');
 const authRoute = require('./routes/auth.route');
 const productRoute = require('./routes/product.route');
 const { errorHandler, notFound } = require('./middlewares');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI;
 
+const connectDB = async () => await mongoose.connect(MONGODB_URI);
+connectDB()
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.use(cors());
 app.use(express.json());
 
 app.use('/auth', authRoute);
